6db58a1e53623d0d209e48702368ddb454d1528d
acer458 <thepropfirmundercover@gmail.com>
2025-08-02 20:25:08 +0530

    Update supabaseClient.js

--- client/src/supabaseClient.js
+++ client/src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://pgkhizgigtcclrkwnjwo.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBna2hpemdpZ3RjY2xya3duandvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNDM0NjIsImV4cCI6MjA2OTcxOTQ2Mn0.Ip11dsvUyeDMbVM0m-Ri2pTkg3yMQeS65MtC8ykVsmw";
+// const supabaseUrl = "https://pgkhizgigtcclrkwnjwo.supabase.co";
+// const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBna2hpemdpZ3RjY2xya3duandvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNDM0NjIsImV4cCI6MjA2OTcxOTQ2Mn0.Ip11dsvUyeDMbVM0m-Ri2pTkg3yMQeS65MtC8ykVsmw";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
