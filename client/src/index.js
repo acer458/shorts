58697d92506fe61e853261197ecb851d8f44f1c2
acer458 <thepropfirmundercover@gmail.com>
2025-08-02 12:41:35 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,3 @@ import App from "./App";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
-html, body, #root {
-  background: #000 !important;
-  padding: 0;
-  margin: 0;
-}
-::-webkit-scrollbar { width: 0; background: transparent; }
