541509e9dd7fa2d1870e0fdda3abe25b527dc3ca
acer458 <thepropfirmundercover@gmail.com>
2025-08-05 02:41:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Feed from "./Feed";
 import VideoPlayer from "./VideoPlayer";
 import AdminDashboard from "./AdminDashboard";
-import { AuthProvider } from "./context/AuthContext"; // <-- New: provide context
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Main (random order, all videos) */}
-          <Route path="/" element={<Feed />} />
-          {/* Single video by filename (direct link) */}
-          <Route path="/shorts/:filename" element={<VideoPlayer />} />
-          {/* Admin */}
-          <Route path="/admin" element={<AdminDashboard />} />
-          {/* Optional: Not found route (uncomment if you have a 404 page) */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        {/* Main (random order, all videos) */}
+        <Route path="/" element={<Feed />} />
+        {/* Single video by filename */}
+        <Route path="/shorts/:filename" element={<VideoPlayer />} />
+        {/* Admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        {/* Optional: Not found route */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
