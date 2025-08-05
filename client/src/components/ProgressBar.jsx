5e62a1cc2068fbcb29e2c35f4afd7675c95d2b57
acer458 <thepropfirmundercover@gmail.com>
2025-08-05 23:54:04 +0530

    Update and rename UserStats.js to ProgressBar.jsx

---
+++ client/src/components/ProgressBar.jsx
@@ -0,0 +1,13 @@
+const ProgressBar = ({ progress }) => {
+  return (
+    <div className="progress-container">
+      <div 
+        className="progress-bar" 
+        style={{ width: `${progress}%` }}
+      ></div>
+      <span className="progress-text">{progress}%</span>
+    </div>
+  );
+};
+
+export default ProgressBar;
