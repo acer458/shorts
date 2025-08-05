b993758289da1288578f9e1a1d863dbe4b7aad05
acer458 <thepropfirmundercover@gmail.com>
2025-08-06 02:40:10 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,3 @@ import AdminDashboard from "./AdminDashboard";
 import { AuthProvider } from "./components/Auth/useAuth";
 // If you have NotFound component, you can import here
 // import NotFound from "./NotFound";
-
-export default function App() {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Main (random order, all videos) */}
-          <Route path="/" element={<Feed />} />
-          {/* Single video by filename */}
-          <Route path="/shorts/:filename" element={<VideoPlayer />} />
-          {/* Admin */}
-          <Route path="/admin" element={<AdminDashboard />} />
-          {/* Optional: Not found route */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
-}
