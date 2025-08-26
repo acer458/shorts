a3dcb07a873945d1e329e37ef50922ed9aab285c
acer458 <thepropfirmundercover@gmail.com>
2025-08-26 19:57:48 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Feed from "./Feed";
 import VideoPlayer from "./VideoPlayer";
 import AdminDashboard from "./AdminDashboard";
-import About from "./About";
+import About from "./pages/About";  // Correct path here
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
         <Route path="/shorts/:filename" element={<VideoPlayer />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<Feed />} />
+        <Route path="*" element={<Feed />} /> {/* Correct catch-all path */}
       </Routes>
     </BrowserRouter>
   );
