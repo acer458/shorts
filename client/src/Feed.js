a4a00a5bca834aa43cb2576adc98bb66afce0af4
Shivam-Ban <notehanmalik@gmail.com>
2025-08-27 09:06:53 +0530

    Update Feed.js

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -907,7 +907,7 @@ export default function Feed() {
         }, SINGLE_DELAY);
       },
     };
-  // }
+  }
   // ---- Seek ----
   function handleSeek(idx, e, isTouch = false) {
     let clientX;
