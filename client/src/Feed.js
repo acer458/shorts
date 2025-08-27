6f0cffc591dea41ae5751eb43d2fd0ff0a3ca861
Shivam-Ban <notehanmalik@gmail.com>
2025-08-28 00:31:44 +0530

    Update Feed.js

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -1466,7 +1466,7 @@ export default function Feed() {
               alignItems: "center",
               gap: 8,
               fontWeight: 600,
-              fontSize: 14,
+              fontSize: 14.5,
               marginBottom: 4.5, // a bit more breathing room above caption
               color: "#0054ff",
             }}
@@ -1476,7 +1476,7 @@ export default function Feed() {
               src={"https://res.cloudinary.com/dzozyqlqr/image/upload/v1754518014/d0d1d9_vp6st3.jpg"}
               alt="PropScholar"
               width={27}
-              height={7}
+              height={27}
               style={{
                 display: "block",
                 borderRadius: "50%",              // perfect circle
