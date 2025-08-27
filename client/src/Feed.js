cb8997814d418ed7f94eb00485a5e6c248f81edb
Shivam-Ban <notehanmalik@gmail.com>
2025-08-27 09:50:02 +0530

    Update Feed.js

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -826,7 +826,7 @@ export default function Feed() {
   function handleVideoEvents(idx, filename) {
     let clickTimer = null;
     let lastTap = 0;
-    const SINGLE_DELAY = 1070;
+    const SINGLE_DELAY = 870;
 
     const likeThenPulse = () => {
       if (!isLiked(filename)) {
